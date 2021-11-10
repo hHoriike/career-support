d360b0549960279e1c0645b272903812ca7f491a
hHoriike <h_horiike@sbs-infosys.co.jp>
2021-11-10 11:38:08 +0900

    FontAwesomeのCDNやめたから不要

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "ress";
 import "./globals.scss";
-import React, { useContext } from "react";
+import React from "react";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 
@@ -11,10 +11,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>静岡仕事図鑑</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <script
-          src="https://kit.fontawesome.com/0fd4b07ae9.js"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
       <Component {...pageProps} />
     </>
